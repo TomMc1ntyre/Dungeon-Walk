e29fa5526ebdeef30ffa9427410666f5408f9800
tommcintyre <N00241674@iadt.ie>
2025-12-10 08:51:19 +0000

    Initial Dungeon Walk site

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+export default {
+  content: ["./index.html", "./src/**/*.{js,css,html}"],
+  theme: {
+    extend: {
+      colors: {
+        brand: "#FFD300",
+        dark: "#0d0d0f"
+      },
+      spacing: {
+        card: "1.25rem",
+        section: "5rem"
+      },
+      fontSize: {
+        hero: "3.2rem"
+      }
+    }
+  },
+  plugins: []
+}
